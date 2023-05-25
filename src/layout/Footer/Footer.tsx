36cb92085f1d6e4545b358664fd7eaff33f452a9
Yanarkaev <hasssanbek@gmail.com>
2023-05-25 20:44:56 +0300

    сверстал футер

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,8 +1,23 @@
 import React, { DetailedHTMLProps, HTMLAttributes } from "react";
+import styles from "./Footer.module.scss";
+import cn from "classnames";
 
 interface IProps
   extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
 
-export const Footer = ({ ...props }: IProps) => {
-  return <footer {...props}>Footer</footer>;
+export const Footer = ({ className, ...props }: IProps) => {
+  return (
+    <footer className={cn(styles.footer, className)} {...props}>
+      <div className={styles.copyrights}>
+        OwlTop &copy; 2020-2023 Все права защищены
+      </div>
+
+        <a href="" className={styles.link}>
+          Пользовательское соглашение
+        </a>
+        <a href="" className={styles.link}>
+          Политика конфиденциальности
+        </a>
+    </footer>
+  );
 };
