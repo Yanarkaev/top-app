9e85fcedbc2becae42e99c8d919cbf0b137f189f
Yanarkaev <hasssanbek@gmail.com>
2023-05-27 02:08:39 +0300

    получил все пути

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,44 @@
 import { Rating } from "@/components/Rating/Rating";
+import { MenuItem } from "@/interfaces/menu.interface";
 import { withLayout } from "@/layout/withLayout";
+import axios from "axios";
+import { GetStaticProps } from "next";
 import { useState } from "react";
 
-const Home = () => {
+const Home = ({ menu }: HomeProps) => {
   const [rating, setRating] = useState<number>(4);
   return (
     <div>
-      <Rating rating={rating} setRating={setRating} isEditable={true} />
+      {/* <Rating rating={rating} setRating={setRating} isEditable={true} /> */}
+      <ul>
+        {menu.map((el) => (
+          <li key={el._id.secondCategory}>{el._id.secondCategory}</li>
+        ))}
+      </ul>
     </div>
   );
 };
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(
+    "https://courses-top.ru" + "/api/top-page/find",
+    {
+      firstCategory
+    }
+  );
+
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
